e4525aa843ec508495c2be97a41e3eba3913f77c
Ezequiel Santos <ezequiel.santos@miniclip.com>
2025-11-27 15:37:27 +0000

    Initial Commit

---
+++ XCFrameworkGeneratorApp/XCFrameworkGeneratorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCFrameworkGeneratorAppApp.swift
+//  XCFrameworkGeneratorApp
+//
+//  Created by Ezequiel dos Santos on 27/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct XCFrameworkGeneratorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCFrameworkGeneratorAppTests/XCFrameworkGeneratorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  XCFrameworkGeneratorAppTests.swift
+//  XCFrameworkGeneratorAppTests
+//
+//  Created by Ezequiel dos Santos on 27/11/2025.
+//
+
+import Testing
+@testable import XCFrameworkGeneratorApp
+
+struct XCFrameworkGeneratorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
